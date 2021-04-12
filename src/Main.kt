2de625d3c7c999c0433edbe7d8d7801fbb708367
CristianGC <cristian.ghinea.1996@gmail.com>
2021-04-12 19:51:14 +0300

    Am eliminat o funcție main și am comentat tot codul vechi din main.

--- src/Main.kt
+++ src/Main.kt
@@ -2,65 +2,172 @@ import core.*
 
 fun main(args: Array<String>)
 {
-    println("Hello Kotlin!")
+    /*
+        println("Hello Kotlin!")
 
-    println(sumaV1(54,57))
-    println(sumaV2(23,45))
-    sumaV3(54,12)
-    sumaV4(34,62)
+        println(sumaV1(54,57))
+        println(sumaV2(23,45))
+        sumaV3(54,12)
+        sumaV4(34,62)
 
-    variablesOnlyForReading()
-    variablesCanBeChanged()
-    multiplication()
-    usingStringTemplates()
+        variablesOnlyForReading()
+        variablesCanBeChanged()
+        multiplication()
+        usingStringTemplates()
 
-    println(maxV1(10,10))
-    println(maxV2(10,10))
-    println(maxV3(10,10))
-    maxV4(10,20)
-    println("Max_v5: ${maxV5(10,11)}")
+        println(maxV1(10,10))
+        println(maxV2(10,10))
+        println(maxV3(10,10))
+        maxV4(10,20)
+        println("Max_v5: ${maxV5(10,11)}")
 
-    typeCheckingCharStringIntBooleanV1(66)
-    typeCheckingCharStringIntBooleanV1('5')
-    typeCheckingCharStringIntBooleanV1("10000")
-    typeCheckingCharStringIntBooleanV1(true)
-    typeCheckingCharStringIntBooleanV1(println("\nHello Kotlin! )))"))
+        typeCheckingCharStringIntBooleanV1(66)
+        typeCheckingCharStringIntBooleanV1('5')
+        typeCheckingCharStringIntBooleanV1("10000")
+        typeCheckingCharStringIntBooleanV1(true)
+        typeCheckingCharStringIntBooleanV1(println("\nHello Kotlin! )))"))
 
-    val numbersV1: IntArray = intArrayOf(5,2,3,4,5,6)
+        val numbersV1: IntArray = intArrayOf(5,2,3,4,5,6)
 
-    maximumNumberOfMatricesV1(numbersV1)
+        maximumNumberOfMatricesV1(numbersV1)
 
-    println(typeCheckingV2(10))
-    println(typeCheckingV2('1'))
-    println(typeCheckingV2(10.10f))
-    println(typeCheckingV2("Cristian"))
-    println(typeCheckingV2(true))
-    println(typeCheckingV2(10.10))
+        println(typeCheckingV2(10))
+        println(typeCheckingV2('1'))
+        println(typeCheckingV2(10.10f))
+        println(typeCheckingV2("Cristian"))
+        println(typeCheckingV2(true))
+        println(typeCheckingV2(10.10))
 
-    usingIntervals()
+        usingIntervals()
 
-    typeCheckingV3(10)
-    typeCheckingV3('1')
-    typeCheckingV3(10.10f)
-    typeCheckingV3("Cristian")
-    typeCheckingV3(true)
-    typeCheckingV3(10.10)
-    typeCheckingV3(Int)
+        typeCheckingV3(10)
+        typeCheckingV3('1')
+        typeCheckingV3(10.10f)
+        typeCheckingV3("Cristian")
+        typeCheckingV3(true)
+        typeCheckingV3(10.10)
+        typeCheckingV3(Int)
 
-    defaultValuesForFunctionParameters(20,"Cristian")
-    defaultValuesForFunctionParameters(10)
-    defaultValuesForFunctionParameters()
+        defaultValuesForFunctionParameters(20,"Cristian")
+        defaultValuesForFunctionParameters(10)
+        defaultValuesForFunctionParameters()
 
-    startColorConversion()
+        startColorConversion()
 
-    printMyObject1()
+        printMyObject1()
 
-    typeCheckingV3(transformFromCharToInt('7'))
+        typeCheckingV3(transformFromCharToInt('7'))
 
-    val arrayForm = arrayOf("Form","9","8","4","3","57","11")
-    var arrayTo = arrayOf("To","8","3","2","5","1","01")
+        val arrayForm = arrayOf("Form","9","8","4","3","57","11")
+        var arrayTo = arrayOf("To","8","3","2","5","1","01")
 
-    copyTheArrayToArray(arrayForm, arrayTo)
+        copyTheArrayToArray(arrayForm, arrayTo)
+    */
+
+    /*
+        val _object0 = Person0()
+        println("_object0: ${_object0.hashCode()}")
+        println("_object0: ${_object0.toString()}")
+        //rintln("_object0: ${_object0.equals()}")
+        val _object1 = Person1()
+        val _object2 = Person2("Cristian",25)
+        val _object3 = Person3("Cristian",25)
+        val _object4 = Person4("Cristian",25)
+        val _object5 = Person5("Cristian",25)
+        //val _object6 = Person6("Cristian",25)
+        //val _object7 = Person7("Cristian",25)
+        val _object8 = Person8("Cristian",25)
+        val _object9 = Person9("Cristian",25)
+        val _object10 = Person10("Cristian",25)
+        val _object11 = Person11("Cristian",25)
+        //Person12()
+        //Person13()
+
+        println("\n_object14:")
+        val _object14 = Person14("Cristian",25)
+        println("_object14 getName: ${_object14.getName()}")
+        println("_object14 getAge: ${_object14.getAge()}")
+        _object14.printlnSpaceGalaxy()
+
+        println("\n_object15:")
+        val _object15 = Person15("Cristian",25)
+        println("_object15 getName: ${_object15.getName()}")
+        println("_object15 getAge: ${_object15.getAge()}")
+        _object15.printlnSpaceGalaxy()
+
+        println("\n_object16:")
+        val _object16 = Person16("Cristian",25)
+        println("_object16 getName: ${_object16.getName()}")
+        println("_object16 getAge: ${_object16.getAge()}")
+
+        val _object17 = Person17()
+        _object17.printlnSpaceGalaxy()
+
+        Person17().PrintlnGalaxy("Home").printlnSpaceGalaxy()
+
+        Person18("Man",20).Println().name().age()
+        Person18("Man",20).displayOfBasicPeople()
+        Person18("Man",20).printlnName()
+        Person18("Man",20).printlnAge()
+        Person18("Man",20).printlnName().printlnAge().displayOfBasicPeople()
+
+        val obj1 = Person19.create()
+        obj1.displayOfBasicPeople()
+
+        val obj2 = Person19.create2()
+        obj2.printlnSpaceGalaxy()
+
+        val obj3 = Person20()
+        obj3.oldFun("Salut")
+        obj3.newFun("Salut")
+
+        ///https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.reflect/-k-class/
+        if (Person20::class.isCompanion)
+        {
+            println("class: Person20 Această clasă are un obiect companion.")
+        }
+
+        obj3.weDisplayOddAndEvenNumbersFromTheList()
+
+        val name = "Sanda"
+        if (obj3.thisIsTheName(name))
+        {
+            println("Acest nume este: $name")
+        }
+
+        obj3.weCheckIfTheWordLengthIsEvenOrOdd()
+
+        obj3.weCheckIfTheWordLengthIsEvenOrOdd2()
+
+        val obj4 = Person21()
+        obj4.function { Person20() }
+
+        val obj5 = Person22()
+        obj5.startLateinitWork()
+        obj5.startLazyWork()
+
+        val obj6 = Person23(10,10)
+        println("obj6.stepNoOne: ${obj6.stepNoOne}")
+        obj6.printlnNumberStep()
+        obj6.printlnIndex1()
+        obj6.printlnIndex2()
+        obj6.printlnIndex3()
+
+        val obj7 = Person24("Cristian",25)
+        obj7.printlnName()
+        obj7.printlnAge()
+        obj7.printlnProfession()
+
+        val obj8 = Person25("Cristian",25)
+        obj8.printlnName()
+        obj8.printlnAge()
+
+        Person25("Cristiam",25).printlnName()
+        Person25("Cristiam",25).printlnAge()
+    */
+
+    val obj9 = Person26("Cristian",25)
+    println("obj9.Index: ${obj9.Index}")
 
 
 
