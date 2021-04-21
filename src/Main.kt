20127a8a1be4dbaef5232deb804869fa59452231
CristianGC <cristian.ghinea.1996@gmail.com>
2021-04-21 04:41:43 +0300

    Principii de baza ale POO :     Abstractizarea     Mostenirea     Polimorfismul     Incapsularea

--- src/Main.kt
+++ src/Main.kt
@@ -202,6 +202,7 @@ fun main(args: Array<String>)
     object17.printlnNameAge()
     */
 
+    /*
     val object18 = Person32("Cristian", "25")
     println(object18.name(object18.getType()))
     println(object18.age(object18.getType()))
@@ -222,4 +223,34 @@ fun main(args: Array<String>)
     println(Test1.test1(Test))
 
     Workers()
+    */
+
+    println("\nCar1")
+    val Car1 : Bus = Car(70.0f, "Space", "Bus-14384")
+    Car1.printlnBus()
+    Car1.BurningTemperature = 40.0f
+    Car1.dieselFuel = 170
+    Car1.StartingTheCar()
+    Car1.StartingTheCar(false)
+    Car1.StartingTheCar(true)
+
+    println("\nCar2")
+    val Car2 : Sedan = Car(140.0f, "VM", "Sedan-57763")
+    Car2.printlnSedan()
+    Car2.BurningTemperature = 50.0f
+    Car2.gasolineFuel = 70
+    Car2.StartingTheCar()
+    Car2.StartingTheCar(false)
+    Car2.StartingTheCar(true)
+
+    println("\nCar3")
+    val Car3 : Cabriolet = Car(200.0f, "STB", "Cabriolet-31374")
+    Car3.printlnCabriolet()
+    Car3.removeTheRoofFromTheCar()
+    Car3.BurningTemperature = 70.0f
+    Car3.gasolineFuel = 50
+    Car3.StartingTheCar()
+    Car3.StartingTheCar(false)
+    Car3.StartingTheCar(true)
+
 }
--- src/core/Core.kt
+++ src/core/Core.kt
@@ -1471,9 +1471,227 @@ class Workers()
     }
 }
 
+/*
+    Principii de baza ale POO :
+    Abstractizarea
+    Mostenirea
+    Polimorfismul
+    Incapsularea
+*/
 
+interface BurningTemperature
+{
+    var BurningTemperature: Float
+}
 
+interface DieselFuel : BurningTemperature
+{
+    var dieselFuel : Int
+}
 
+interface GasolineFuel : BurningTemperature
+{
+    var gasolineFuel : Int
+}
 
+interface StartingTheCar
+{
+    fun StartingTheCar()
+    fun StartingTheCar(stop: Boolean)
+}
 
+interface Bus : DieselFuel, StartingTheCar
+{
+    fun printlnBus()
+}
 
+interface Sedan : GasolineFuel, StartingTheCar
+{
+    fun printlnSedan()
+}
+
+interface Cabriolet : GasolineFuel, StartingTheCar
+{
+    fun printlnCabriolet()
+    fun removeTheRoofFromTheCar()
+}
+
+abstract class Vehicle()
+{
+    protected var maxSpeed = 0.0f
+        get() = field
+        set(value) {field = setMaxSpeed(value, 200)}
+
+    private fun setMaxSpeed(value : Float, max : Int) : Float
+    {
+        if (value <= max)
+        {
+            println("setMaxSpeed")
+        }
+        else
+        {
+            throw Exception("setMaxSpeed: Nu pot fi setate valori mai mari de ${max}")
+        }
+
+        return value
+    }
+
+    protected var carBrand : String = ""
+        get() = field
+        set(value) {field = setMaximumNumberOf_Characters(value, 10)}
+
+    protected var carNumber : String = ""
+        get() = field
+        set(value) {field = setMaximumNumberOf_Characters(value, 20)}
+
+    private fun setMaximumNumberOf_Characters(value : String, max: Int) : String
+    {
+        if (value.length < max)
+        {
+            println("setMaximumNumberOf_Characters")
+        }
+        else
+        {
+            throw Exception("setMaximumNumberOf_Characters: Nu pot fi setate valori mai mari de ${max}")
+        }
+
+        return value
+    }
+
+    private fun fuelPumping()
+    {
+        println("\n 1: fuelPumping")
+    }
+
+    private fun startingTheStarter()
+    {
+        println(" 2: startingTheStarter")
+    }
+
+    private fun weTurnOnTheFuel()
+    {
+        println(" 3: weTurnOnTheFuel")
+    }
+
+    private fun startingTheEngine()
+    {
+        fuelPumping()
+        startingTheStarter()
+        weTurnOnTheFuel()
+        println(" 4: startingTheEngine")
+    }
+
+    open fun StartingTheCar()
+    {
+        startingTheEngine()
+        println(" 5: StartingTheCar")
+    }
+}
+
+class Car(maxSpeed: Float, carBrand: String, carNumber: String) : Vehicle(), Bus, Sedan, Cabriolet
+{
+    init
+    {
+        println("\nCar:")
+        this.maxSpeed = maxSpeed
+        this.carBrand = carBrand
+        this.carNumber = carNumber
+
+        println("maxSpeed: ${maxSpeed}")
+        println("carBrand: ${carBrand}")
+        println("carNumber: ${carNumber}")
+    }
+
+    override fun printlnBus()
+    {
+        println("printlnBus")
+    }
+
+    override var dieselFuel: Int = 0
+        get() = field
+        set(value) {field = setDieselFuel(value)}
+
+    fun setDieselFuel(value: Int): Int
+    {
+        if (value <= 200)
+        {
+            println("setDieselFuel")
+        }
+        else
+        {
+            throw Exception("dieselFuel: Nu pot fi setate valori mai mari de 200")
+        }
+
+        return value
+    }
+
+    override fun printlnSedan()
+    {
+        println("printlnSedan")
+    }
+
+    override fun printlnCabriolet()
+    {
+        println("printlnCabriolet")
+    }
+
+    override fun removeTheRoofFromTheCar()
+    {
+        println("removeTheRoofFromTheCar")
+    }
+
+    override var gasolineFuel: Int = 0
+        get() = field
+        set(value) {field = setGasolineFuel(value)}
+
+    fun setGasolineFuel(value: Int): Int
+    {
+        if (value < 100)
+        {
+            println("setGasolineFuel")
+        }
+        else
+        {
+            throw Exception("gasolineFuel: Nu pot fi setate valori mai mari de 100")
+        }
+
+        return value
+    }
+
+    override var BurningTemperature: Float = 0.0f
+        get() = field
+        set(value) {field = setBurningTemperature(value)}
+
+    fun setBurningTemperature(value: Float): Float
+    {
+        if (value < 90)
+        {
+            println("setBurningTemperature")
+        }
+        else
+        {
+            throw Exception("BurningTemperature: Nu pot fi setate valori mai mari de 90")
+        }
+
+        return value
+    }
+
+    override fun StartingTheCar()
+    {
+        super.StartingTheCar()
+        println("Mașina a fost creată de ură.")
+    }
+
+    override fun StartingTheCar(stop: Boolean)
+    {
+        if (stop == false)
+        {
+            println("Din anumite motive, a refuzat să pornească mașina.")
+        }
+        else
+        {
+            super.StartingTheCar()
+            println("Mașina a fost creată de ură.")
+        }
+    }
+}
