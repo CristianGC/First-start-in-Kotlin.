002a5f152ac2b039b960efe333e399670bb3c92c
CristianGC <cristian.ghinea.1996@gmail.com>
2021-04-23 19:13:16 +0300

    Am eliminat codul de care nu aveam nevoie în Core.kt.

--- src/Main.kt
+++ src/Main.kt
@@ -2,33 +2,33 @@ import core.*
 
 fun main()
 {
-    val Car1 : Bus = Car(70.0f, "Space", "Bus-14384")
-    val Car2 : Sedan = Car(140.0f, "VM", "Sedan-57763")
-    val Car3 : Cabriolet = Car(200.0f, "STB", "Cabriolet-31374")
+    val car1 : Bus = Car(70.0f, "Space", "Bus-14384")
+    val car2 : Sedan = Car(140.0f, "VM", "Sedan-57763")
+    val car3 : Cabriolet = Car(200.0f, "STB", "Cabriolet-31374")
 
     println("\nCar1")
-    Car1.printlnBus()
-    Car1.BurningTemperature = 40.0f
-    Car1.dieselFuel = 170
-    Car1.StartingTheCar()
-    Car1.StartingTheCar(false)
-    Car1.StartingTheCar(true)
+    car1.printlnBus()
+    car1.burningTemperature = 40.0f
+    car1.dieselFuel = 170
+    car1.StartingTheCar()
+    car1.StartingTheCar(false)
+    car1.StartingTheCar(true)
 
     println("\nCar2")
-    Car2.printlnSedan()
-    Car2.BurningTemperature = 50.0f
-    Car2.gasolineFuel = 70
-    Car2.StartingTheCar()
-    Car2.StartingTheCar(false)
-    Car2.StartingTheCar(true)
+    car2.printlnSedan()
+    car2.burningTemperature = 50.0f
+    car2.gasolineFuel = 70
+    car2.StartingTheCar()
+    car2.StartingTheCar(false)
+    car2.StartingTheCar(true)
 
     println("\nCar3")
-    Car3.printlnCabriolet()
-    Car3.removeTheRoofFromTheCar()
-    Car3.BurningTemperature = 70.0f
-    Car3.gasolineFuel = 50
-    Car3.StartingTheCar()
-    Car3.StartingTheCar(false)
-    Car3.StartingTheCar(true)
+    car3.printlnCabriolet()
+    car3.removeTheRoofFromTheCar()
+    car3.burningTemperature = 70.0f
+    car3.gasolineFuel = 50
+    car3.StartingTheCar()
+    car3.StartingTheCar(false)
+    car3.StartingTheCar(true)
     
 }
