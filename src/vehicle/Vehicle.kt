cf3dca3dedba1bf739f77bcfe2522a88c57a5a53
Cristian <cristian.ghinea.1996@gmail.com>
2021-04-26 08:51:56 +0300

    Am șters 2 fișiere și ceva cod.

--- src/vehicle/Vehicle.kt
+++ src/vehicle/Vehicle.kt
@@ -4,27 +4,4 @@ abstract class Vehicle
 {
 	open val maxSpeedKmH = 10000.0f
 	
-	
-	private fun fuelPumping()
-	{
-		println("\n 1: fuelPumping")
-	}
-	
-	private fun startingTheStarter()
-	{
-		println(" 2: startingTheStarter")
-	}
-	
-	private fun weTurnOnTheFuel()
-	{
-		println(" 3: weTurnOnTheFuel")
-	}
-	
-	private fun startingTheEngine()
-	{
-		fuelPumping()
-		startingTheStarter()
-		weTurnOnTheFuel()
-		println(" 4: startingTheEngine")
-	}
 }
