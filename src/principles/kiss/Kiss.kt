839fb1f98ea37bb576bb9c01386ac24b84edc22a
Cristian <cristian.ghinea.1996@gmail.com>
2021-05-08 11:43:19 +0300

    Am încălcat principiul KISS.

---
+++ src/principles/kiss/Kiss.kt
@@ -0,0 +1,26 @@
+package principles.kiss
+
+class Kiss
+{
+	private  lateinit var kiss : Any
+	
+	fun setKiss(obj : Any)
+	{
+		kiss = obj
+	}
+	
+	fun Kiss(obj : Any)
+	{
+		kiss = obj
+	}
+	
+	fun setK(obj : Any)
+	{
+		kiss = obj
+	}
+	
+	fun setKi(obj : Any)
+	{
+		kiss = obj
+	}
+}
