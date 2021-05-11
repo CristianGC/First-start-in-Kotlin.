5dfc10ead10a353ef118581902c1666805e7b4bf
Cristian <cristian.ghinea.1996@gmail.com>
2021-05-11 14:34:29 +0300

    Java Polymorphism

---
+++ src/startINjava/polymorphism/Dog.java
@@ -0,0 +1,9 @@
+package startINjava.polymorphism;
+
+public class Dog extends Animal {
+
+    @Override
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
---
+++ src/startINjava/polymorphism/Polymorphism.java
@@ -0,0 +1,14 @@
+package startINjava.polymorphism;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        Animal myAnimal = new Animal();
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
