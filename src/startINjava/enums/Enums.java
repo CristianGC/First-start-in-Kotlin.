ec65165fddc2b4ca30efb692886507e0205c733a
Cristian <cristian.ghinea.1996@gmail.com>
2021-05-11 15:14:06 +0300

    Java Enums 1 Enum inside a Class 2 Enum in a Switch Statement 3 Loop Through an Enum

---
+++ src/startINjava/enums/Enums.java
@@ -0,0 +1,23 @@
+package startINjava.enums;
+
+public class Enums {
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+
+        switch(myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+
+        for (Level myVar2 : Level.values()) {
+            System.out.println(myVar2);
+        }
+    }
+}
