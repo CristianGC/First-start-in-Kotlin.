d597f4f27c93ffb8c0991998d60e5411c4cab9a9
Cristian <cristian.ghinea.1996@gmail.com>
2021-05-11 14:49:55 +0300

    Java Abstraction 1 Abstract class 2 Abstract method

---
+++ src/startINjava/abstraction/Abstraction.java
@@ -0,0 +1,9 @@
+package startINjava.abstraction;
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Animal myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
